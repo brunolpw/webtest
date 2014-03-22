87f04e1e1063390ae3081021a6dc1574718ccddc
Bruno <brunolpw@outlook.com>
2014-03-22 00:40:41 -0300

    novo ajuste

--- js/location.js
+++ js/location.js
@@ -9,3 +9,20 @@ function showPosition() {
 function getPosition(posicao) {
 	area.innerHTML = "Latitude: " + posicao.coords.latitude + "<br /> Longitude: " + posicao.coords.longitude;
 }
+
+function geoError( err ) {
+	switch( err.code ){
+		case 1:
+	 		x.innerHTML="Geolocalização rejeitada pelo usuário.";
+			break;
+		case 2:
+			x.innerHTML="Localização indisponível.";
+			break;
+		case 3:
+		x.innerHTML="A requisição expirou.";
+		break;
+		case 0:
+		x.innerHTML="Algum erro desconhecido aconteceu.";
+		break;
+	}	
+};
