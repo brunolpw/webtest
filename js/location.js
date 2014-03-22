c7716e24c2bd486762df19809213854b4f0b6595
Bruno <brunolpw@outlook.com>
2014-03-22 01:30:15 -0300

    Acho que agora vai

--- js/location.js
+++ js/location.js
@@ -1,41 +1,51 @@
 ﻿// JavaScript Document
-var area = document.getElementById("geoarea");
-var btnHere = document.getElementById( "btn-here" );
-var lat, lon;
+var x = document.getElementById("pagina");
+var btnHere = document.getElementById("btn-here");
 
 btnHere.addEventListener("click",showPosition());
+
+function getLocation(){
+	
+if (navigator.geolocation){ navigator.geolocation.getCurrentPosition(showPosition,showError); }
+  else{ x.innerHTML = "Infelizmente foi detectado um problema de incapacidade."; }
+}
  
-function showPosition() {
-	alert("procurando...");
-	//if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(getPosition);
-		area.innerHTML = "Latitude: " + lat + "<br> Longitude: " + lon;
-		alert("Latitude: " + lat + "<br> Longitude: " + lon);
-	//}
-	//else{ area.innerHTML = "Infelizmente este browser não suporta o recurso de geolocalização."; }
+function showPosition(position){
+	
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	latlon = new google.maps.LatLng(lat, lon)
+	mapholder = document.getElementById('mapa')
+	
+	mapholder.style.height='250px';
+	mapholder.style.width='500px';
+	 
+	var myOptions = {
+		center:latlon,zoom:14,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		mapTypeControl:false,
+		navigationControlOptions:{ style:google.maps.NavigationControlStyle.SMALL }
+	};
+	
+	var map = new google.maps.Map(document.getElementById("mapa"),myOptions);
+	var marker = new google.maps.Marker({position:latlon,map:map,title:"Você está Aqui!"});
 }
-
-function getPosition(posicao) {
-	lat = posicao.coords.latitude, lon = posicao.coords.longitude;
-}
-
-function show(){
-	area.innerHTML = "Latitude: " + lat + "<br> Longitude: " + lon;
-}
-
-function geoError( err ) {
-	switch( err.code ){
-		case 1:
-	 		x.innerHTML="Geolocalização rejeitada pelo usuário.";
+ 
+// Tratando erros.
+function ErrorSolutions(error){
+	
+	switch(error.code){
+		case error.PERMISSION_DENIED:
+			x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
 			break;
-		case 2:
-			x.innerHTML="Localização indisponível.";
+		case error.POSITION_UNAVAILABLE:
+			x.innerHTML="Localização indisponível."
 			break;
-		case 3:
-		x.innerHTML="A requisição expirou.";
-		break;
-		case 0:
-		x.innerHTML="Algum erro desconhecido aconteceu.";
-		break;
-	}	
-};
+		case error.TIMEOUT:
+			x.innerHTML="O tempo da requisição expirou."
+			break;
+		case error.UNKNOWN_ERROR:
+			x.innerHTML="Algum erro desconhecido aconteceu."
+			break;
+	}
+}
