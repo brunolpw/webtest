24d63d4241099d554425cbc4159f68742f21347b
Bruno <brunolpw@outlook.com>
2014-03-22 00:25:47 -0300

    teste de localização

---
+++ js/location.js
@@ -0,0 +1,11 @@
+﻿// JavaScript Document
+var area = document.getElementById("geoarea");
+ 
+function ExibirLocalizacao() {
+	if(navigator.geolocation){ navigator.geolocation.getCurrentPosition(ObterPosicao); }
+	else{ area.innerHTML = "Infelizmente este browser não suporta o recurso de geolocalização."; }
+}
+ 
+function ObterPosicao(posicao) {
+	area.innerHTML = "Latitude: " + posicao.coords.latitude + "<br /> Longitude: " + posicao.coords.longitude;
+}
---
+++ js/newconnection.js
@@ -0,0 +1,17 @@
+﻿// JavaScript Document
+
+function geoSuccess( pos ) {
+	// armazena as coordenadas de latitude e longitude
+	var lat = pos.coords.latitude, lng = pos.coords.longitude;
+};
+
+function geoError( err ) {
+	switch( err.code ) {
+		case 1: x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."; break;
+		case 2: x.innerHTML="Localização indisponível."; break; x.innerHTML="Localização indisponível."; break;
+		case 3: x.innerHTML="A requisição expirou."; break;
+		case 0: x.innerHTML="Algum erro desconhecido aconteceu."; break;
+	}	
+};
+
+navigator.geolocation.getCurrentPosition( geoSuccess );
